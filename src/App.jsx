29127c1fbb0b8b5afde13c6dbebcb33d7a18726a
anus26 <anusraza268gmail.com>
2025-05-26 18:11:45 -0700

    singnup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Left from './Home/Left/Left'
 
-import Singnup from './Components/singnup'
+import Right from './Home/Right/Right'
+import Singnup from './Components/Singnup'
 import Login from './Components/Login'
 import { useAuth } from './context/AuthProvider'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import  { Toaster } from 'react-hot-toast';
-import Right from './Home/Right/Right'
 
 const App = () => {
   const [authUser,setAuthUser]=useAuth()
