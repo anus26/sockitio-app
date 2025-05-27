51ee71c7bfe6f9a9c827c45c0550c098174a1fac
anus26 <anusraza268gmail.com>
2025-05-26 18:08:40 -0700

    right

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Left from './Home/Left/Left'
-import Right from './Home/right/right'
+
 import Singnup from './Components/singnup'
 import Login from './Components/Login'
 import { useAuth } from './context/AuthProvider'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import  { Toaster } from 'react-hot-toast';
+import Right from './Home/Right/Right'
 
 const App = () => {
   const [authUser,setAuthUser]=useAuth()
