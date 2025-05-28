3e2683ccf41771f3a9dbb93fbbb5a80611845f8f
anus26 <anusraza268gmail.com>
2025-05-27 20:15:52 -0700

    modified

--- src/Components/Singnup.jsx
+++ src/Components/Singnup.jsx
@@ -38,7 +38,7 @@ const Signup = () => {
             toast.success("successfully registerd")
         }
         localStorage.setItem("chatapp",JSON.stringify(response.data))
-        Cookies.set("jwt",JSON.stringify(response.data))
+    Cookies.set("jwt", response.data.token);        
         setAuthUser(response.data)
 
     })
