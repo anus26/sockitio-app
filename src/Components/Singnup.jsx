91b6a7bf678deaf8be67783036178c2262b9b9a2
anus26 <anusraza268gmail.com>
2025-05-27 19:56:00 -0700

    modified

--- src/Components/Singnup.jsx
+++ src/Components/Singnup.jsx
@@ -4,7 +4,8 @@ import { useForm } from "react-hook-form";
 import { useAuth } from '../context/AuthProvider';
 import { Link, useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
-
+import js from '@eslint/js';
+import Cookies from "js-cookie"
 
 const Signup = () => {
     const [authUser,setAuthUser]= useAuth()
@@ -37,6 +38,7 @@ const Signup = () => {
             toast.success("successfully registerd")
         }
         localStorage.setItem("chatapp",JSON.stringify(response.data))
+        Cookies.set("jwt",response.data)
         setAuthUser(response.data)
 
     })
