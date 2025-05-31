41d109b255a9e49aa32d1b8287f72f656282dc02
anus26 <anusraza268gmail.com>
2025-05-30 23:42:36 -0700

    modified

--- src/Home/Right/Right.jsx
+++ src/Home/Right/Right.jsx
@@ -13,13 +13,13 @@ const Right = () => {
 
   return (
     // <div className="bg-blue-900  w-[70%] text-gray-300 flex flex-col h-screen md:hidden sm:hidden lg:block xl:block  ">
-  <div className={`lg:block ${selectedChat ? "block":"hidden"} w-full  bg-blue-900 text-gray-300 flex flex-1 overflow-y-auto flex-col lg:w-[70%]`}   
-                style={{ maxHeight: "calc(92vh - 8vh)" }}> 
+  <div className={`lg:block ${selectedChat ? "block":"hidden"} w-full  bg-blue-900 text-gray-300 flex  overflow-y-auto flex-col lg:w-[70%]`}   
+                style={{ maxHeight: "calc(129vh - 10vh)" }}> 
       {!selectedChat ? <NoChat /> : (
         <>
           <Chatuser />
 
-          <div className="flex-1 overflow-y-auto px-4" style={{ maxHeight: 'calc(100vh - 14vh)' }}>
+          <div className="flex-1 overflow-y-auto px-4" >
             <Message />
           </div>
           <div className="border-t border-blue-800">
@@ -37,7 +37,7 @@ const NoChat = () => {
   const [authUser] = useAuth();
 
   return (
-    <div className="flex items-center justify-center h-full text-center px-4 sm:hidden md:hidden   lg:block xl:block">
+    <div className="flex items-center justify-center h-full text-center px-4 sm:hidden md:hidden   lg:block xl:block"    >
       <div className=''>
         <h1 className="text-2xl font-semibold mb-2">
           Welcome, <span className="text-white font-bold">{authUser?.user?.fullname || "Guest"}</span>
